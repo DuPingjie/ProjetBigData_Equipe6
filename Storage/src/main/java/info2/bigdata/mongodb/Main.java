843be041c0c3319aaace356ca1d4b55a66bc402f
YangDi666 <32524847+YangDi666@users.noreply.github.com>
2019-02-14 08:48:08 -0500

    Update Main.java

--- Storage/src/main/java/info2/bigdata/mongodb/Main.java
+++ Storage/src/main/java/info2/bigdata/mongodb/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {    
 		
-		DataReader dr=new DataReader ("src/main/resources/dataset.csv");
+		DataReader dr=new DataReader ("src/main/resources/predict.csv");
 		dr.insertToMongo(300000); // parameter: the number of lines to insert (1 - 300000)
 		System.out.println("Data have been put in MongoDB successfully!");
 	}
